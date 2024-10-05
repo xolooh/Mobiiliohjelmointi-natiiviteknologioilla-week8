582a28d7252541f799c4fc3c5d3ab89b1bccb337
xolooh <tapsukk@hotmail.fi>
2024-10-05 11:28:34 +0300

    ready

--- app/src/main/java/com/example/week8/viewmodel/TodoViewModel.kt
+++ app/src/main/java/com/example/week8/viewmodel/TodoViewModel.kt
@@ -6,20 +6,22 @@ import androidx.lifecycle.viewModelScope
 import com.example.week8.Model.Todo
 import com.example.week8.api.TodosApi
 import kotlinx.coroutines.launch
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class TodoViewModel: ViewModel() {
-    var todos = mutableListOf<Todo>()
-    private set
+    var todos = mutableStateListOf<Todo>()
+        private set
 
     init {
-getTodosList()
+        getTodosList()
     }
 
     private fun getTodosList() {
         viewModelScope.launch {
-            var todosApi: TodosApi? = null
             try {
-                todosApi = TodosApi!!.getInstance()
+                val todosApi = TodosApi.getInstance()
                 todos.clear()
                 todos.addAll(todosApi.getTodos())
             } catch (e: Exception) {
